56a7ed970941fc39ec41a1cf5364e658f19a1d7b
MarcinRo1234 <154052364+MarcinRo1234@users.noreply.github.com>
2023-12-17 11:27:01 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,26 @@
+const form = document.querySelector("form");
+const ul = document.querySelector("ul")
+const taskNumber = document.querySelector("h1 span");
+const listItems = document.getElementsByClassName("task");
+const input = document.querySelector("input");
+
+const removeTask = (e) => {
+    e.target.parentNode.remove();
+    taskNumber.textContent = listItems.length;
+}
+
+const addTask = (e) => {
+    e.preventDefault();
+    const titleTask = input.value;
+    if(titleTask === "") return;
+    const task = document.createElement("li");
+    task.className = "task";
+    task.innerHTML = titleTask + "<button>Usuń</buton>"
+    ul.appendChild(task);
+    input.value = "";
+    // const liItems = document.querySelectorAll(li.task).length; długie i nie potrzebne
+    taskNumber.textContent = listItems.length;
+    task.querySelector("button").addEventListener("click", removeTask);
+};
+
+form.addEventListener("submit", addTask);
