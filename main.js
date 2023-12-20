0ea6506751eedd4af356614a1db44b9eae809db9
MarcinRo1234 <154052364+MarcinRo1234@users.noreply.github.com>
2023-12-20 11:15:02 +0100

    Update main.js

--- main.js
+++ main.js
@@ -4,6 +4,10 @@ const taskArrayList = document.querySelector(".tasks-array-list")
 
 let counter = 0;
 
+const removeLiElement = (e) => {
+    e.target.parentNode.remove();
+    console.log("działą");
+}
 
 const taskAdd = () => {
     let inputValue = input.value;
@@ -13,19 +17,24 @@ const taskAdd = () => {
     counter++
     let liElement = document.createElement("li");
     liElement.classList.add("tasks-array-list-element");
-    liElement.innerHTML = counter +"." +"  "+ inputValue +   `<i class="fa-solid fa-check"></i>` +   `<i class="fa-solid fa-x"></i>`;
+    // liElement.innerHTML = counter +"." +"  "+ inputValue +   `<i class="fa-solid fa-check"></i>` +   `<i class="fa-solid fa-x"></i>`;
+    liElement.innerHTML = counter + "." +"  " + inputValue + `<button class = "done">Zrobione</button>`
     // liElement.textContent = counter +"   "+ inputValue;
-
+    liElement.querySelector("button").addEventListener("click", removeLiElement);
     taskArrayList.appendChild(liElement);
 
 
     input.value="";
     console.log(counter);
+    // const btnli =liElement.getElementsByClassName("fa-check");
+
+
+    // btnli.addEventListener("click", removeLiElement)
 };
 
 
 
-
+console.log(counter);
 
 
 
