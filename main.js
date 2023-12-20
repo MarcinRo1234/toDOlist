4a6d7e67b1e55e9992b7a6cdb9a3f139c558ec42
MarcinRo1234 <154052364+MarcinRo1234@users.noreply.github.com>
2023-12-20 15:07:32 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,16 @@
 const input = document.querySelector("input");
 const btnAdd = document.querySelector(".btn-add");
 const taskArrayList = document.querySelector(".tasks-array-list")
+const pCounter = document.querySelector(".counter");
+const btnEnd = document.querySelector(".btn-end");
 
 let counter = 0;
+let decrease = 0;
 
 const removeLiElement = (e) => {
     e.target.parentNode.remove();
+    decrease++;
+    pCounter.textContent = `Liczba wykonanych zadań : ${decrease}`;
     console.log("działą");
 }
 
@@ -18,11 +23,11 @@ const taskAdd = () => {
     let liElement = document.createElement("li");
     liElement.classList.add("tasks-array-list-element");
     // liElement.innerHTML = counter +"." +"  "+ inputValue +   `<i class="fa-solid fa-check"></i>` +   `<i class="fa-solid fa-x"></i>`;
-    liElement.innerHTML = counter + "." +"  " + inputValue + `<button class = "done">Zrobione</button>`
+    liElement.innerHTML = counter + "." +"  " + inputValue + `<button class = "done">&#9989</button>`
     // liElement.textContent = counter +"   "+ inputValue;
     liElement.querySelector("button").addEventListener("click", removeLiElement);
     taskArrayList.appendChild(liElement);
-
+    
 
     input.value="";
     console.log(counter);
@@ -32,7 +37,13 @@ const taskAdd = () => {
     // btnli.addEventListener("click", removeLiElement)
 };
 
-
+const endWork = () => {
+    let data = new Date();
+    console.log(data);
+    let NewDate = `${data.getDate()} : ${data.getMonth() + 1} : ${data.getFullYear()}`
+    console.log(NewDate);
+    localStorage.setItem(NewDate, decrease);
+}
 
 console.log(counter);
 
@@ -40,3 +51,4 @@ console.log(counter);
 
 
 btnAdd.addEventListener("click", taskAdd)
+btnEnd.addEventListener("click", endWork);
